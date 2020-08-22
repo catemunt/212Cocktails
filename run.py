a8fb2b9e0cfd843e6d6a205f761706fa5c40ba2b
Cate <catemunt@gmail.com>
2020-08-22 20:28:52 +1200

    finishing

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def fetchMenu(con):
         sides.append(list(row))
 
     ingredients = []
-    cur = con.execute('SELECT ingredient, ingredient FROM ingredients')
+    cur = con.execute('SELECT id, ingredient FROM ingredients')
     for row in cur:
         ingredients.append(list(row))
 
@@ -42,30 +42,54 @@ def index():
                             ingredients=menu['ingredients']
                             )
 
-@app.route('/order')
-def order():
-    con = sqlite3.connect(MENUDB)
-    menu = fetchMenu(con)
-    con.close()
-
-    return render_template('order.html',
-                            disclaimer='may contain traces of nuts',
-                            burgers=menu['burgers'],
-                            drinks=menu['drinks'],
-                            sides=menu['sides'],
-                            ingredients=menu['ingredients']
-                            )
+# @app.route('/order')
+# def order():
+#     con = sqlite3.connect(MENUDB)
+#     menu = fetchMenu(con)
+#     con.close()
+#
+#     return render_template('order.html',
+#                             disclaimer='may contain traces of nuts',
+#                             burgers=menu['burgers'],
+#                             drinks=menu['drinks'],
+#                             sides=menu['sides'],
+#                             ingredients=menu['ingredients']
+#                             )
 
 @app.route('/confirm', methods=['POST'])
 def confirm():
+    alcohols = ''
+    count = 0
+
+    for k, v in request.form.items():
+        # if request.form.[input]:
+            # alcohols[input] = request.form[input]
+        if count == 0:
+            alcohols = alcohols + '(ingredients.id)=' + k
+            count +=1
+        else:
+            alcohols = alcohols + ' OR ' + '(ingredients.id)=' + k
+
+
+    print(alcohols)
+
+    con = sqlite3.connect(MENUDB)
+    menu = fetchMenu(con)
+    cur = con.execute('SELECT DISTINCT cocktails.name, cocktails.image FROM ingredients INNER JOIN (cocktails INNER JOIN cocktailIngredients ON cocktails.id = cocktailIngredients.cocktailid) ON ingredients.id = cocktailIngredients.ingredientsid WHERE ((' + alcohols + '))')
+
+    records = cur.fetchall()
+    print(records)
+
+    con.close()
     items = {}
 
     for input in request.form:
-        if request.form[input] and request.form[input] != 0:
+        if request.form[input]:
             items[input] = request.form[input]
 
     print(items)
 
 
     return render_template('confirm.html',
-                            items = items)
+                            items = items,
+                            records = records)
